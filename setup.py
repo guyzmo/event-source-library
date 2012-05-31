15fbb3b98cd698a29f173c87f014528c911e44dc
Guyzmo <guyzmo+github@m0g.net>
2012-05-31 18:26:25 +0200

    version increment

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ News
 """ % read('README', 'CHANGES')
 
 setup(name="eventsource",
-      version="1.0.1",
+      version="1.0.2",
       description="Event Source Library",
       long_description=long_description,
       author="Bernard Pratz",
